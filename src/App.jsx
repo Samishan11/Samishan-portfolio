f96b71e66a24e146298808a2e1c5b07805183fbd
Samishan11 <samishanthapa0@gmail.com>
2023-03-25 12:20:03 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
+function App() {
+  return (
+    <Router>
+      <CustomCursor
+        strokeWidth={10}
+        targets={[".link", ".your-css-selector"]}
+        customClass="custom-cursor"
+        dimensions={50}
+        opacity ={0.5}
+        fill="#FCB900"
+        strokeColor={"#FCB900"}
+        scale={0.1}
+        smoothness={{
+          movement: 0.05,
+          scale: 0.1,
+          opacity: 0.5,
+        }}
+        targetOpacity={0.5}
+      />
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/navbar/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Sidebar = () => {
+  return (
+    <div className="w-[80px] bg-gray-900 text-white text-center fixed top-0 left-0 h-screen">
+      <div className="logo bg-orange-500 py-5">
+        <h1 className="font-extrabold text-4xl">ST</h1>
+      </div>
+      <ul className="list-none text-center text-xl">
+        <li className="pt-16 pb-5 border-b border-gray-600"><i className="fa-solid fa-home"></i></li>
+        <li className="py-5 border-b border-gray-600"> <i className="fa-solid fa-user"></i></li>
+        <li className="py-5 border-b border-gray-600"><i class="fa-solid fa-file"></i> </li>
+        <li className="py-5 border-b border-gray-600"><i class="fa-solid fa-briefcase"></i> </li>
+        <li className="py-5 border-b border-gray-600"><i className="fa-solid fa-blog"></i></li>
+        <li className="py-5"><i className="fa-solid fa-phone-volume"></i></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
